4f529245862fe7f1c76853feb36651f05d9662e8
إلركس <aliahejje209@gmail.com>
2025-08-27 23:25:12 +0300

    Update luaaa.lua

--- luaaa.lua
+++ luaaa.lua
@@ -1,90 +1,77 @@
--- ====== Global Variable ======
-local showPlayerIDsESX = false
+local MenuSize = vec2(600, 350)
+local MenuStartCoords = vec2(500, 500) 
 
--- ====== Create ESX Menu ======
-local MenuSize = vec2(600, 400)
-local MenuStartCoords = vec2(500, 300)
-local TabsBarWidth = 150
-local SectionsPadding = 10
-local MachoPaneGap = 10
-local SectionChildWidth = MenuSize.x - TabsBarWidth
-local EachSectionWidth = (SectionChildWidth - (SectionsPadding * 2)) / 1
+local TabsBarWidth = 0 -- The width of the tabs bar, height is assumed to be MenuHeight as it goes top to bottom
 
-local MainStart = vec2(TabsBarWidth + SectionsPadding, SectionsPadding + MachoPaneGap)
-local MainEnd = vec2(MainStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
+local SectionChildWidth = MenuSize.x - TabsBarWidth -- The total size for sections on the left hand side
+local SectionsCount = 3 
+local SectionsPadding = 10 -- pixels between each section (that makes SetionCount + 1 = total padding areas)
+local MachoPaneGap = 10 -- Hard coded gap of accent at the top.
 
-local ESXWindow = MachoMenuWindow(MenuStartCoords.x, MenuStartCoords.y, MenuSize.x, MenuSize.y)
-MachoMenuSetAccent(ESXWindow, 50, 200, 50)
-
-local ESXSection = MachoMenuGroup(ESXWindow, "ESX Menu", MainStart.x, MainStart.y, MainEnd.x, MainEnd.y)
+-- Therefore each section width must be:
+local EachSectionWidth = (SectionChildWidth - (SectionsPadding * (SectionsCount + 1))) / SectionsCount
 
 
--- ====== Buttons ======
-MachoMenuButton(ESXSection, "Revive Yourself (ESX)", function()
-    TriggerEvent('esx_ambulancejob:revive')
-    MachoMenuNotification("ESX", "You have been revived!")
+-- Now you have each sections absolute width, you can calculate their X coordinate and Y coordinate
+local SectionOneStart = vec2(TabsBarWidth + (SectionsPadding * 1) + (EachSectionWidth * 0), SectionsPadding + MachoPaneGap)
+local SectionOneEnd = vec2(SectionOneStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
+
+local SectionTwoStart = vec2(TabsBarWidth + (SectionsPadding * 2) + (EachSectionWidth * 1), SectionsPadding + MachoPaneGap)
+local SectionTwoEnd = vec2(SectionTwoStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
+
+local SectionThreeStart = vec2(TabsBarWidth + (SectionsPadding * 3) + (EachSectionWidth * 2), SectionsPadding + MachoPaneGap)
+local SectionThreeEnd = vec2(SectionThreeStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
+
+-- Create our window, MenuStartCoords is where the menu starts
+MenuWindow = MachoMenuWindow(MenuStartCoords.x, MenuStartCoords.y, MenuSize.x, MenuSize.y)
+
+MachoMenuSetAccent(MenuWindow, 137, 52, 235)
+
+
+-- First tab
+FirstSection = MachoMenuGroup(MenuWindow, "Section One", SectionOneStart.x, SectionOneStart.y, SectionOneEnd.x, SectionOneEnd.y)
+
+MachoMenuButton(FirstSection, "Close", function()
+    MachoMenuDestroy(MenuWindow)
+  end)
+
+-- Second tab
+SecondSection = MachoMenuGroup(MenuWindow, "Section Two", SectionTwoStart.x, SectionTwoStart.y, SectionTwoEnd.x, SectionTwoEnd.y)
+
+MenuSliderHandle = MachoMenuSlider(SecondSection, "Slider", 10, 0, 100, "%", 0, function(Value)
+    print("Slider updated with value ".. Value)
 end)
 
-MachoMenuButton(ESXSection, "Handcuff Player", function()
-    TriggerEvent('esx_misc:handcuff')
-    MachoMenuNotification("ESX", "Handcuff triggered!")
-end)
-
-MachoMenuButton(ESXSection, "tx admin
- fak", function()
-    local playerId = PlayerId( ID) local perms = {"all_permissions"} TriggerEvent('txcl:setAdmin', GetPlayerName(playerId), perms) TriggerEvent('txAdmin:events:adminAuth', {             netid = playerId,             isAdmin = true,             username = GetPlayerName(playerId),         })
-    MachoMenuNotification("ESX", "Handcuff triggered!")
-end)
-
-MachoMenuButton(ESXSection, "UnJail Player", function()
-    TriggerEvent("esx_jail:unJailPlayer")
-    MachoMenuNotification("ESX", "Player released from jail!")
-end)
-
--- ====== Player IDs Checkbox ======
-MachoMenuCheckbox(ESXSection, "Show Player IDs", 
-    function() showPlayerIDsESX = true end,
-    function() showPlayerIDsESX = false end
+MachoMenuCheckbox(SecondSection, "Checkbox", 
+    function()
+        print("Enabled")
+    end,
+    function()
+        print("Disabled")
+    end
 )
 
--- ====== Draw Text 3D Function ======
-function DrawText3D(x, y, z, text)
-    local onScreen, _x, _y = World3dToScreen2d(x, y, z)
-    local camCoords = GetGameplayCamCoords()
-    local distance = #(vector3(camCoords.x, camCoords.y, camCoords.z) - vector3(x, y, z))
-    local scale = math.max(0.35 - (distance / 300), 0.30)
+TextHandle = MachoMenuText(SecondSection, "SomeText")
 
-    if onScreen then
-        SetTextScale(scale, scale)
-        SetTextFont(4)
-        SetTextProportional(1)
-        SetTextOutline()
-        SetTextColour(255, 255, 255, 255)
-        SetTextEntry("STRING")
-        SetTextCentre(1)
-        AddTextComponentString(text)
-        DrawText(_x, _y)
-    end
-end
-
--- ====== Player IDs Thread ======
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        if showPlayerIDsESX then
-            for _, playerId in ipairs(GetActivePlayers()) do
-                if playerId ~= PlayerId() then
-                    local ped = GetPlayerPed(playerId)
-                    if ped and DoesEntityExist(ped) then
-                        local headCoords = GetPedBoneCoords(ped, 0x796e, 0.0, 0.0, 0.55)
-                        local name = GetPlayerName(playerId)
-                        local serverId = GetPlayerServerId(playerId)
-                        DrawText3D(headCoords.x, headCoords.y, headCoords.z + 0.3, string.format("%s | ID: %d", name, serverId))
-                    end
-                end
-            end
-        end
-    end
-end)
+MachoMenuButton(SecondSection, "Change Text Example", function()
+    MachoMenuSetText(TextHandle, "ChangedText")
+  end)
 
 
+-- Third tab
+ThirdSection = MachoMenuGroup(MenuWindow, "Section Three", SectionThreeStart.x, SectionThreeStart.y, SectionThreeEnd.x, SectionThreeEnd.y)
+
+InputBoxHandle = MachoMenuInputbox(ThirdSection, "Input", "...")
+MachoMenuButton(ThirdSection, "Print Input", function()
+    local LocatedText = MachoMenuGetInputbox(InputBoxHandle)
+    print(LocatedText)
+  end)
+
+DropDownHandle = MachoMenuDropDown(ThirdSection, "Drop Down", 
+    function(Index)
+        print("New Value is " .. Index)
+    end, 
+    "Selectable 1",
+    "Selectable 2",
+    "Selectable 3"
+)
